e8bd93c33d871b1647a7c4a10784924a5a0f005a
xzhichen <xzhichen@umich.edu>
2018-04-09 14:56:08 -0400

    first commit

---
+++ gitexample/gitexample/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  gitexample
+//
+//  Created by Zhicheng Xie on 4/9/18.
+//  Copyright © 2018 Zhicheng Xie. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
